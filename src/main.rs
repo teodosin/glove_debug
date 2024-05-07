647a3f54baeaf5608710b5be396a74ca0b346482
Viktor Teodosin <teodosin@protonmail.com>
2024-05-07 13:13:58 +0300

    Compiles and tries to connect to esp32

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,94 @@
-use bevy::prelude::*;
-
-use dbus::blocking::Connection;
-use std::time::Duration;
-
-extern crate hidapi;
-
-use hidapi::HidApi;
-
+mod asyncs;
 mod ble;
+
+use asyncs::{TaskContext, TokioTasksPlugin, TokioTasksRuntime};
+use bevy::prelude::*;
+use btleplug::api::{Central, Manager as _, Peripheral, ScanFilter};
+use btleplug::platform::Manager;
+use std::time::Duration;
+use tokio::time;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(TokioTasksPlugin::default())
         .add_systems(Startup, connect)
         .add_systems(Update, listen)
     .run();
 }
 
-fn connect() {
+fn connect(runtime: ResMut<TokioTasksRuntime>, mut commands: Commands) {
     // do the bluetooth connection thingy
+    runtime.spawn_background_task(try_connect);
 
-    match HidApi::new() {
-        Ok(api) => {
-            for device in api.device_list() {
-                println!("-------------------------------");
-                println!("{:04x}:{:04x}", device.vendor_id(), device.product_id());
-                println!("{:?}", device.path());
-                println!("{:?}", device.serial_number());
-                println!("{:?}", device.product_string().unwrap());
-            }
-        },
-        Err(e) => {
-            eprintln!("Error: {}", e);
-        },
-    }
-
-    println!("-------------------- Hidapi before, dbus after ------------------");
-
-    let conn = Connection::new_session().unwrap();
-
-    // Second, create a wrapper struct around the connection that makes it easy
-    // to send method calls to a specific destination and path.
-    let proxy = conn.with_proxy("org.freedesktop.DBus", "/", Duration::from_millis(5000));
-
-    // Now make the method call. The ListNames method call takes zero input parameters and
-    // one output parameter which is an array of strings.
-    // Therefore the input is a zero tuple "()", and the output is a single tuple "(names,)".
-    let (names,): (Vec<String>,) = proxy.method_call("org.freedesktop.DBus", "ListNames", ()).unwrap();
-
-    // Let's print all the names to stdout.
-    for name in names { println!("{}", name); }
 }
 
 fn listen() {
     // nothing right now
+}
+
+async fn try_connect(mut ctx: TaskContext) {
+    let manager = Manager::new().await.expect("Failed to create BLE manager");
+    let adapter_list = manager.adapters().await.expect("Failed to get adapter list");
+    if adapter_list.is_empty() {
+        eprintln!("No Bluetooth adapters found");
+        return;
+    }
+
+    for adapter in adapter_list.iter() {
+        println!("Starting scan on {}...", adapter.adapter_info().await.expect("Failed to get adapter info"));
+        adapter
+            .start_scan(ScanFilter::default())
+            .await
+            .expect("Can't scan BLE adapter for connected devices...");
+        time::sleep(Duration::from_secs(10)).await;
+        let peripherals = adapter.peripherals().await.expect("Failed to get peripherals");
+        if peripherals.is_empty() {
+            eprintln!("->>> BLE peripheral devices were not found, sorry. Exiting...");
+        } else {
+            // All peripheral devices in range
+            for peripheral in peripherals.iter() {
+                let properties = peripheral.properties().await.expect("Failed to get peripheral properties");
+                let is_connected = peripheral.is_connected().await.expect("Failed to get connection status");
+                let local_name = properties
+                    .unwrap()
+                    .local_name
+                    .unwrap_or(String::from("(peripheral name unknown)"));
+                println!(
+                    "Peripheral {:?} is connected: {:?}",
+                    local_name, is_connected
+                );
+                if !is_connected {
+                    println!("Connecting to peripheral {:?}...", &local_name);
+                    if let Err(err) = peripheral.connect().await {
+                        eprintln!("Error connecting to peripheral, skipping: {}", err);
+                        continue;
+                    }
+                }
+                let is_connected = peripheral.is_connected().await.expect("Failed to get connection status");
+                println!(
+                    "Now connected ({:?}) to peripheral {:?}...",
+                    is_connected, &local_name
+                );
+                peripheral.discover_services().await.expect("Failed to discover services");
+                println!("Discover peripheral {:?} services...", &local_name);
+                for service in peripheral.services() {
+                    println!(
+                        "Service UUID {}, primary: {}",
+                        service.uuid, service.primary
+                    );
+                    for characteristic in service.characteristics {
+                        println!("  {:?}", characteristic);
+                    }
+                }
+                if is_connected {
+                    println!("Disconnecting from peripheral {:?}...", &local_name);
+                    peripheral
+                        .disconnect()
+                        .await
+                        .expect("Error disconnecting from BLE peripheral");
+                }
+            }
+        }
+    }
 }
